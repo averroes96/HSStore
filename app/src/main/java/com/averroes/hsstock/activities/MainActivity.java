64352e375fde712b0b38a64544f35b9d09b8e843
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2020-12-08 00:26:12 +0100

    Filtering

--- app/src/main/java/com/averroes/hsstock/activities/MainActivity.java
+++ app/src/main/java/com/averroes/hsstock/activities/MainActivity.java
@@ -13,7 +13,9 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -24,6 +26,8 @@ import com.averroes.hsstock.adapters.CustomAdapter;
 import com.averroes.hsstock.database.DBHandler;
 import com.averroes.hsstock.models.Product;
 import com.averroes.hsstock.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView sum,typeTV;
     private ImageButton goToDepot,refsIB;
     private RecyclerView productList;
+    private FloatingActionButton filterFAB;
 
 
     private DBHandler dbHandler;
@@ -56,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         goToDepot = findViewById(R.id.goToDepot);
         typeTV = findViewById(R.id.typeTV);
         refsIB = findViewById(R.id.refsIB);
+        filterFAB = findViewById(R.id.filterFAB);
 
         typeTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,6 +117,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        filterFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFilterDialog();
+            }
+        });
+
         //dbHandler.ignoreThis();
         
         storeData();
@@ -121,6 +134,83 @@ public class MainActivity extends AppCompatActivity {
         customAdapter.notifyDataSetChanged();
 
         sum.setText(customAdapter.getItemCount() + " Chaussure(s)");
+    }
+
+    private void showFilterDialog() {
+
+        final BottomSheetDialog dialog = new BottomSheetDialog(this);
+
+        final View view = LayoutInflater.from(this).inflate(R.layout.filter_layout, null);
+        dialog.setContentView(view);
+
+        ImageButton backBtn;
+        final TextView typeTV,sizeTV;
+        EditText colorET;
+        Button filterBtn;
+
+        backBtn = view.findViewById(R.id.backBtn);
+        typeTV = view.findViewById(R.id.typeTV);
+        sizeTV = view.findViewById(R.id.sizeTV);
+        colorET = view.findViewById(R.id.colorET);
+        filterBtn = view.findViewById(R.id.filterBtn);
+
+        dialog.show();
+
+        typeTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                typeFiltering();
+            }
+
+            private void typeFiltering() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setTitle(getString(R.string.type_dialog_msg))
+                        .setItems(R.array.types, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String[] array = getResources().getStringArray(R.array.types);
+                                typeTV.setText(array[i]);
+                            }
+                        })
+                        .create().show();
+            }
+        });
+
+        sizeTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sizeFiltering();
+            }
+
+            private void sizeFiltering() {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                    builder.setTitle(getString(R.string.size_dialog_msg))
+                            .setItems(R.array.sizes, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    String[] array = getResources().getStringArray(R.array.sizes);
+                                    sizeTV.setText(array[i]);
+                                }
+                            })
+                            .create().show();
+
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        filterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 
 
---
+++ app/src/main/java/com/averroes/hsstock/adapters/RefsAdapter.java
@@ -0,0 +1,4 @@
+package com.averroes.hsstock.adapters;
+
+public class RefsAdapter {
+}
